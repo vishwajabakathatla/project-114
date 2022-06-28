7cc0015246abdb36568dfaabc6c9cfd1332511a9
vishwajabakathatla <88708444+vishwajabakathatla@users.noreply.github.com>
2022-06-28 17:48:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,17 +2,36 @@ function preload() {
 }
 
 function setup() {
-    canvas=createCanvas(290, 290);
-    canvas.position(575, 250);
-    video=createCapture(VIDEO);
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
     video.hide();
-    tint_color="";
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('poseNet Is Initialized')
 }
 
-
 function draw() {
+    image(video, 0, 0, 300, 300);
 }
 
 function take_snapshot() {
-    save('myLipstickImage.png');
+    save('mylipstickImage.png');
+
+
+}
+
+function gotPoses(results) {
+    {
+
+        if (results.length > 0);
+    
+    console.log(results);
+    console.log("nose x = " + results[0].pose.nose.x);
+    console.log("nose Y = " + results[0].pose.nose.y); 
+    }
 }
